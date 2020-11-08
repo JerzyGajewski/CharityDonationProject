ec45f6d6b9342982e04b57686fc12273954cc437
JerzyGajewski <jerzygajewski3@gmail.com>
2020-11-08 12:08:34 +0100

    created project adn added thymeleaf

---
+++ src/main/java/pl/coderslab/charity/HomeController.java
@@ -0,0 +1,17 @@
+package pl.coderslab.charity;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class HomeController {
+
+
+   @GetMapping()
+    public String homeAction(Model model){
+        return "index";
+    }
+}
